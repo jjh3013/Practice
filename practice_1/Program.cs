cff30ccfa157baf801018df1c8617dfe009a692e
jhjung <jhjung@unitech.co.kr>
2021-04-01 16:23:22 +0900

    1주차 프로그램

---
+++ practice_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputNum = Console.ReadLine();
+            string[] _InputNum = InputNum.Split(",");
+            int count = 0;
+            string Result = null;
+            int[] answer = new int[_InputNum.Length];
+
+            for (int i = 0; i < _InputNum.Length; i++)
+            {
+                if(i > 0)
+                {
+                    Result += ",";
+                }
+                count = 0;
+                int FN = Convert.ToInt32(_InputNum[i]);
+                for(int j = i+1; j < _InputNum.Length; j++)
+                {
+                    int SN = Convert.ToInt32(_InputNum[j]);
+                    if(FN > SN)
+                    {
+                        if(j < _InputNum.Length)
+                        {
+                            count++;
+                        }
+                        break;
+                    }
+                    count++;
+                }
+                Result += count;
+                answer[i] = count;
+            }
+
+            Console.WriteLine(Result);
+        }
+    }
+}
+
+public class Solution
+{
+    public int[] solution(int[] prices)
+    {
+        int[] answer = new int[prices.Length];
+        int count = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            count = 0;
+            int FN = prices[i];
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                int SN = prices[j];
+                if (FN > SN)
+                {
+                    if (j < prices.Length)
+                    {
+                        count++;
+                    }
+                    break;
+                }
+                count++;
+            }
+            answer[i] = count;
+        }
+
+        return answer;
+    }
+}
---
+++ practice_2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자열 입력
+            string InputNum = Console.ReadLine();
+            //쉼표로 자름
+            string[] _InputNum = InputNum.Split(",");
+            //몇번째 숫자를 찾는지 추적
+            int location = Convert.ToInt32(Console.ReadLine());
+            //결과 int형 저장용
+            int[] answer = new int[_InputNum.Length];
+
+            //int형배열로 변환
+            for (int i = 0; i < _InputNum.Length; i++)
+            {
+                answer[i] = Convert.ToInt32(_InputNum[i]);
+            }
+
+            int len = answer.Length;
+            int[] result = new int[len];
+
+            //정렬해줌
+            for(int i = 0; i < len-1; i++)
+            {
+                //if(location < i)
+                //{
+                //    break;
+                //}
+                for(int j = i+1; j < len; j++)
+                {
+                    if(answer[i] < answer[j])
+                    {
+                        if(location == i)
+                        {
+                            location = len - 1;
+                        }
+                        else
+                        {
+                            location--;
+                        }
+                        result[len - 1] = answer[i];
+                        Array.Copy(answer, i + 1, result, i, len - i - 1);
+                        Array.Copy(result, i, answer, i, len - i);
+                        j = i;
+                    }
+                }
+            }
+
+            //결과출력
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+            Console.WriteLine(location+1);
+
+
+        }
+    }
+}
+
+
+
+public class Solution
+{
+    public int solution(int[] priorities, int location)
+    {
+
+        int[] answer = priorities;
+        int len = answer.Length;
+        int[] result = new int[len];
+
+        for (int i = 0; i < len - 1; i++)
+        {
+            if (location < i)
+            {
+                break;
+            }
+            for (int j = i + 1; j < len; j++)
+            {
+                if (answer[i] < answer[j])
+                {
+                    if (location == i)
+                    {
+                        location = len - 1;
+                    }
+                    else
+                    {
+                        location--;
+                    }
+                    result[len - 1] = answer[i];
+                    Array.Copy(answer, i + 1, result, i, len - i - 1);
+                    Array.Copy(result, i, answer, i, len - i);
+                    j = i;
+                }
+            }
+        }
+        location++;
+        return location;
+    }
+}
