7f97c9a11cc2f6dd426427c3d5f16acc1826301f
jhjung <jhjung@unitech.co.kr>
2021-04-20 16:30:55 +0900

    스킬트리작업중

---
+++ SkillTree/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SkillTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자열 입력
+            string Skill = Console.ReadLine();
+            string InputSkillT = Console.ReadLine();
+            //쉼표로 자름
+            string[] Skill_Trees = InputSkillT.Split(",");
+
+            Console.WriteLine(solution(Skill, Skill_Trees));
+
+
+            int solution(string skill, string[] skill_trees)
+            {
+                int answer = 0;
+                int SkillPoint = 0;
+
+                for (int i = 0; i < skill_trees.Length; i++)
+                {
+                    SkillPoint = 0;
+                    for (int j = 0; j < skill_trees[i].Length; j++)
+                    {
+                    }
+                }
+
+
+                return answer;
+            }
+
+        }
+    }
+}
