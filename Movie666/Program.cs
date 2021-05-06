4fa0c93a892dceaf6eadb6a26dc7d0f1f75c3514
jhjung <jhjung@unitech.co.kr>
2021-05-06 12:27:58 +0900

    브루트포스 / 종말영화 해결

---
+++ Movie666/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Movie666
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int N = 0;
+            int answer = 666;
+            string s_answer;
+            string c_answer;
+
+            while(true)
+            {
+                s_answer = Convert.ToString(answer);
+                c_answer = s_answer.Split("666")[0];
+
+                if(s_answer != c_answer)
+                {
+                    N++;
+                    if(N == Num)
+                    {
+                        break;
+                    }
+                }
+                answer++;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
