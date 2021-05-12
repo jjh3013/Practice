8e8e91a1faa7cff9e26b0c61c282b730d1819186
jhjung <jhjung@unitech.co.kr>
2021-05-12 17:47:29 +0900

    정렬 123번

---
+++ NumSort_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace NumSort_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int[] sortNum = new int[Num];
+
+            StringBuilder S_Build = new StringBuilder();
+            
+            for (int i = 0; i < Num; i++)
+            {
+                sortNum[i] = Convert.ToInt32(Console.ReadLine());
+                //for(int j = 0; j < i; j++)
+                //{
+                //    if(sortNum[i] < sortNum[j])
+                //    {
+                //        int temp = sortNum[i];
+                //        Array.Copy(sortNum, j, sortNum, j + 1, Num - j - 1);
+                //        sortNum[j] = temp;
+                //        break;
+                //    }
+                //}
+            }
+
+            Array.Sort(sortNum);
+
+            //sortNum = sortNum.OrderBy(c => c).ToArray();
+
+            foreach (int value in sortNum)
+            {
+                //Console.WriteLine(value);
+                S_Build.Append(value + "\n");
+            }
+
+
+            Console.WriteLine(S_Build);
+        }
+    }
+}
---
+++ Statistics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int[] sortNum = new int[Num];
+            int Sum = 0;
+
+            for (int i = 0; i < Num; i++)
+            {
+                sortNum[i] = Convert.ToInt32(Console.ReadLine());
+                Sum += sortNum[i];
+            }
+        }
+    }
+}
